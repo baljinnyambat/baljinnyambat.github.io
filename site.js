a9d9903fdb3958e35823e54f873a72fe6f8f252c
magsarjav <magsarjav@gmail.com>
2025-11-03 23:03:49 +0800

    bubble color revised

--- site.js
+++ site.js
@@ -4,21 +4,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // follow mouse with transform translate (GPU-accelerated)
   document.addEventListener("mousemove", (e) => {
-    cursor.style.transform = `translate(${e.clientX - 12}px, ${
-      e.clientY - 12
+    cursor.style.transform = `translate(${e.clientX - 8}px, ${
+      e.clientY - 8
     }px)`;
   });
 
   // enlarge when hovering a link or image in a link
   document.addEventListener("mouseover", (e) => {
     if (e.target.closest("a") || e.target.closest(".project")) {
-      bubble.style.transform = "scale(1.6)";
+      bubble.style.transform = "scale(3)";
+      bubble.style.opacity = "0.8";
     }
   });
   document.addEventListener("mouseout", (e) => {
     const related = e.relatedTarget;
     if (!related || (!related.closest("a") && !related.closest(".project"))) {
       bubble.style.transform = "scale(1)";
+      bubble.style.opacity = "1";
     }
   });
 });
